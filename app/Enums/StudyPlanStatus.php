43c5a42391834569d03af6ddd58a04882fdde47a
eman sulaeman <052023036@institutbanten.ac.id>
2025-04-27 13:01:25 +0700

    enum feestatus

---
+++ app/Enums/StudyPlanStatus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums;
+
+use Illuminate\Support\Collection;
+
+enum StudyPlanStatus: string
+{
+    case PENDING = 'Pending';
+    case APPROVED = 'Approve';
+    case REJECT = 'Reject';
+
+    public static function options(): array
+    {
+        return collect(self::cases())->map(fn($item) => [
+            'value' => $item->value,
+            'label' => $item->value,
+        ])->values()->toArray();
+    }
+}
